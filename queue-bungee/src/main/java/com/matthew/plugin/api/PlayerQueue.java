17316c0405ed3d5f3f2ca483b5eb6e1a5768fecc
Matthew <matthew.mcroberts22@gmail.com>
2024-07-16 00:39:55 -0400

    just doing comments

--- queue-bungee/src/main/java/com/matthew/plugin/api/PlayerQueue.java
+++ queue-bungee/src/main/java/com/matthew/plugin/api/PlayerQueue.java
@@ -3,17 +3,53 @@ package com.matthew.plugin.api;
 import com.matthew.plugin.queue.QueuedPlayer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+
 public interface PlayerQueue {
 
+    /**
+     * Adds a player to the queue.
+     *
+     * @param player The player to be added to the queue.
+     */
     void addPlayer(ProxiedPlayer player);
 
+    /**
+     * Retrieves and removes the next player from the queue, waiting if necessary
+     * until an element becomes available.
+     *
+     * @return The next player in the queue.
+     * @throws InterruptedException if interrupted while waiting.
+     */
     QueuedPlayer getNextPlayer() throws InterruptedException;
 
+    /**
+     * Returns the number of players currently in the queue.
+     *
+     * @return The size of the queue.
+     */
     int getSize();
 
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise.
+     */
     boolean isEmpty();
 
+    /**
+     * Returns the position of a specific player in the queue.
+     *
+     * @param player The player whose position is to be determined.
+     * @return The position of the player in the queue, or -1 if the player is not in the queue.
+     */
     int getPlayerPosition(ProxiedPlayer player);
 
+    /**
+     * Finds and returns a specific player from the queue.
+     *
+     * @param player The player to be found.
+     * @return The queued player if found, null otherwise.
+     */
     QueuedPlayer find(ProxiedPlayer player);
 }
+
