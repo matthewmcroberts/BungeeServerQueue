5c63f238a4a751cfa18a0fe5fc68746e4eb7b3a9
Matthew <matthew.mcroberts22@gmail.com>
2024-07-16 12:26:32 -0400

    laying out listener

--- queue-bungee/src/main/java/com/matthew/plugin/listeners/QueueListener.java
+++ queue-bungee/src/main/java/com/matthew/plugin/listeners/QueueListener.java
@@ -1,22 +1,63 @@
 package com.matthew.plugin.listeners;
 
-import com.matthew.plugin.modules.manager.ServerModuleManager;
+import com.matthew.plugin.modules.manager.ModuleManager;
 import com.matthew.plugin.modules.queue.QueueModule;
+import com.matthew.plugin.queue.QueuedPlayer;
 import lombok.RequiredArgsConstructor;
-import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.*;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 @RequiredArgsConstructor
 public class QueueListener implements Listener {
 
     private final Plugin plugin;
+    private final QueueModule module = ModuleManager.getInstance().getRegisteredModule(QueueModule.class);
 
-    private final QueueModule module = ServerModuleManager.getInstance().getRegisteredModule(QueueModule.class);
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPreLogin(PreLoginEvent event) {
+        // Example logic for PreLoginEvent
+        // You could use this event to perform any pre-login checks or preparations if needed
+    }
 
-    @EventHandler
-    public void onConnect(ServerConnectEvent event) {
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPostLogin(PostLoginEvent event) {
+        ProxiedPlayer player = event.getPlayer();
+        // Example logic for PostLoginEvent
+        // Automatically add player to a default queue or based on some logic
+        module.addPlayer("default_server", player);
+    }
 
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onServerConnect(ServerConnectEvent event) throws InterruptedException {
+        ProxiedPlayer player = event.getPlayer();
+        String serverName = event.getTarget().getName();
+
+        // Check if the player is in the queue for the target server
+        QueuedPlayer queuedPlayer = module.find(serverName, player);
+        if (queuedPlayer != null) {
+            // If the player is in the queue, perform actions like reserving a spot or managing priorities
+            // Remove the player from the queue after they successfully connect to the server
+            module.getNextPlayer(serverName);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onServerKick(ServerKickEvent event) {
+        ProxiedPlayer player = event.getPlayer();
+        String serverName = event.getKickedFrom().getName();
+
+        // Handle the case where a player is kicked from a server
+        // Optionally, re-add the player to the queue or handle based on your queue logic
+        module.addPlayer(serverName, player);
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerDisconnect(ServerDisconnectEvent event) {
+        ProxiedPlayer player = event.getPlayer();
+        // Remove the player from all queues they might be in
     }
 }
